6775771f77f09479fed0a7ea817bd41d818dc705
Bennet Witczak <bennet.witczak@icloud.com>
2023-10-11 10:37:49 +0200

    Fixed Bugs

--- js/classes/statusbarpoisen.class.js
+++ js/classes/statusbarpoisen.class.js
@@ -1,11 +1,11 @@
 class StatusBarPoisen extends DrawableObject {
   IMAGES = [
-    "../img/4. Marcadores/orange/0_ copia.png",
-    "../img/4. Marcadores/orange/20_ copia.png",
-    "../img/4. Marcadores/orange/40_ copia.png",
-    "../img/4. Marcadores/orange/60_ copia.png",
-    "../img/4. Marcadores/orange/80_ copia.png",
-    "../img/4. Marcadores/orange/100_ copia.png",
+    "./img/4. Marcadores/orange/0_ copia.png",
+    "./img/4. Marcadores/orange/20_ copia.png",
+    "./img/4. Marcadores/orange/40_ copia.png",
+    "./img/4. Marcadores/orange/60_ copia.png",
+    "./img/4. Marcadores/orange/80_ copia.png",
+    "./img/4. Marcadores/orange/100_ copia.png",
   ];
 
   playerPoisen = 0;
